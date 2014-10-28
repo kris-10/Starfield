83a1ce857db4050b295be0183a446181bad7069f
unknown <krleung@697-334-Shelob.sf.k12.internal>
2014-10-28 11:22:50 -0700

    made it!

--- build-tmp/source/Starfield.java
+++ build-tmp/source/Starfield.java
@@ -1,10 +1,26 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Starfield extends PApplet {
+
 // //your code here
 // Particle[] oddPart = new Particle [1000];
 
 // JumboParticle[] biggie = new JumboParticle[15];
 
 Particle[] part = new Particle [2000];
-void setup()
+public void setup()
 {
 	size(800,800);//your code here
 	
@@ -40,7 +56,7 @@ void setup()
 // 		biggie[i].show();
 // 	}	
 // }
-void draw()
+public void draw()
 {
 	background(0);
 	for (int i = 0;i<part.length;i++)
@@ -130,3 +146,12 @@ class JumboParticle extends NormalParticle
 
 
 
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Starfield" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
